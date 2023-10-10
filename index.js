44f00d4d8ce7ad650e188da7d631fbbbdc78cf02
Vishnu <vishnu_ot@yahoo.com>
2023-10-10 11:24:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const { add, div, sub } = require("./utils");
+
+let sum = add(9, 10);
+console.log("Sum of two numbers", sum);
+
+let substraction = sub(15, 10);
+console.log("Substraction answer ", substraction);
+
+let division = div(15, 2);
+console.log("Division answer ", division);
---
+++ utils.js
@@ -0,0 +1,15 @@
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+const div = (num1, num2) => {
+  return num1 / num2;
+};
+
+const sub = (num1, num2) => {
+  return num1 - num2;
+};
+
+
+
+module.exports = {add, div, sub}
